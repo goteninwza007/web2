94b5b5750446dcb664c796e51dfd2792faae924f
goteninwza007 <73677460+goteninwza007@users.noreply.github.com>
2020-11-06 17:35:47 +0700

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Card Image</h2>
+        <br>
+<div class="row">
+        <div class="card col-sm-4">
+          <img class="card-img-top" src="viwe.jpg" alt="Card image" style="width:100%;padding: 0px;">
+          <div class="card-body">
+            <h4 class="card-title">Goten</h4>
+            <p class="card-text">A man overtime falls in love with the woman he is attracted to, and a woman overtime becomes more attracted to the man she loves.</p>
+          </div>
+        </div>
+
+        <div class="card col-sm-4">
+            <img class="card-img-top" src="viwe.jpg" alt="Card image" style="width:100%;padding: 0px;">
+            <div class="card-body">
+              <h4 class="card-title">Taw</h4>
+              <p class="card-text">Friendship is love minus sex and plus reason. Love is friendship plus sex and minus reason.</p>
+            </div>
+        </div>
+
+        <div class="card col-sm-4">
+            <img class="card-img-top" src="viwe.jpg" alt="Card image" style="width:100%;padding: 0px;">
+            <div class="card-body">
+              <h4 class="card-title">KKK</h4>
+              <p class="card-text">You know when you love someone when you want them to be happy even if their happiness means that you’re not part of it.</p>
+            </div>
+        </div>
+</div>
+      </div>
+</body> 
+</html>
